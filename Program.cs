d039b804843910fdab74555964bf743412bc5948
nurcin <nurchin.14@gmail.com>
2024-10-26 15:03:59 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        public static string RemoveSpaces(string input)
+        {
+            return input.Replace(" ", ""); 
+        }
+
+        static void Main()
+        {
+            string str = "Code Academy"; 
+            Console.WriteLine("Original String: " + str);
+
+          
+            str = RemoveSpaces(str);
+
+   
+            Console.WriteLine("String without spaces: " + str);
+            Console.ReadLine();
+        }
+    }
+}
